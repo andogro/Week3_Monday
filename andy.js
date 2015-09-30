a01ac4f15bdc710a7e4d0731bb07d5e60a9dc18e
andogro <andogro@gmail.com>
2015-09-30 10:41:04 -0600

    Update andy.js

--- andy.js
+++ andy.js
@@ -221,10 +221,14 @@ isNumber(arrayOfAllTheThings);
 
 // 15. Create a die rolling function that accepts two parameters (the two six-sided dice) and outputs an array of the two values rolled.
 
+var die1 =  Math.floor(Math.random()*6 + 1);
+var die2 =  Math.floor(Math.random()*6 + 1);
+var dieThrow =[];
 var die = function(x,y) {
-return [x,y];
-}
-console.log (die(2,3));
+dieThrow = [x,y];
+ };
+die(die1,die2);
+console.log(dieThrow);
 
 // 16. Create another fuction called "whichSide". It will accept the array from above as it's only parameter.
 //     This function will mimic a Monopoly board. Start playerOne at the Go space (0) and add the two values from your die array to determine
